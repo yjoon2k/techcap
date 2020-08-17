a208057934a6e8a94b1ab47532169595b8ae2462
jang_eun_ju <dmswn0261@ajou.ac.kr>
2020-08-18 04:23:23 +0900

    eunju

--- frontend/src/mypage.vue
+++ frontend/src/mypage.vue
@@ -1,17 +1,68 @@
 <template>
     <div>
-        <p1 id="p1">fdsfds</p1>
+        <div>
+            <button id="home" class="btn">홈</button>
+            <button id="inform" class="btn">내 정보 변경</button>
+            <button id="secure" class="btn">보안</button>
+            <button id="class" class="btn">내 수업</button>
+            <button id="student" class="btn">내 학생</button>
+            <button id="teacher" class="btn">내 선생님</button>
+            <button id="home" class="btn">홈</button>
+            <button id="inform" class="btn">내 정보 변경</button>
+            <button id="secure" class="btn">보안</button>
+            <button id="class" class="btn">내 수업</button>
+            <button id="student" class="btn">내 학생</button>
+            <button id="teacher" class="btn">내 선생님</button>
+        </div>
+        <div id="body">
+            <h1 id="mp">마이페이지</h1>
+            <img id="image" src="@/assets/image.png">
+           
+        </div>
+
     </div>
 </template>
 
 <script>
-export default {
-  name: 'mypage'
-}
 </script>
-<style>
-    #p1{
+<style> 
+    #mp{
+        font-size: 50px;
         position: absolute;
-        top:400px;
+        left: 800px;
+        top: 50px
+    }
+    #image{
+        width: 200px;
+        height: 200px;
+        position: absolute;
+        left: 818px;
+        top: 200px;
+    }
+    .btn{
+        width: 200px;
+        height: 50px;
+        position: absolute;
+        left: 50px;
+        background-color: white;
+        font-size: 20px;
+    }
+    #home{
+        top:250px;
+    }
+    #inform{
+        top: 300px;
+    }
+    #secure{
+        top: 350px;
+    }
+    #class{
+        top: 400px;
+    }
+    #student{
+        top: 450px;
+    }
+    #teacher{
+        top: 500px;
     }
 </style>
